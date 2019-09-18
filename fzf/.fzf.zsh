4966d3bb38d560c53de753f8c111a867dc817a72
Raza Farooq <razaf88@gmail.com>
2019-09-18 05:54:05 -0400

    - Fixed error on fzf

--- fzf/.fzf.zsh
+++ fzf/.fzf.zsh
@@ -1,13 +1,13 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */home/canaan/.fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/home/canaan/.fzf/bin"
+if [[ ! "$PATH" == *"${HOME}"/.fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}${HOME}/.fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/home/canaan/.fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "${HOME}/.fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/home/canaan/.fzf/shell/key-bindings.zsh"
+source "${HOME}/.fzf/shell/key-bindings.zsh"
