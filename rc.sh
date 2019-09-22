8741022797ddd42a5fe72b6dab3a7684d5c05676
Raza Farooq <razaf88@gmail.com>
2019-09-22 06:53:40 -0400

    - Output git pull to null

--- rc.sh
+++ rc.sh
@@ -3,7 +3,7 @@
 # if shell is in interactive-mode
 if [ -t 0 ]; then
 	echo loading and updating dotfiles
-	git -C "${HOME}"/dotfiles reset --hard HEAD
-	git -C "${HOME}"/dotfiles pull --rebase
+	git -C "${HOME}"/dotfiles reset --quiet --hard HEAD
+	git -C "${HOME}"/dotfiles pull --quiet --rebase
 	bash "${HOME}"/dotfiles/install.sh
 fi
