fa4793ab464335105aa58f632a42bebfba58c436
Raza Farooq <razaf88@gmail.com>
2019-09-22 06:59:00 -0400

    - Reverted rc file

--- rc.sh
+++ rc.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-# if you place this file in ~/.ssh/rc, then this script will run everytime someone will log in
-# if shell is in interactive-mode
-if [ -t 0 ]; then
-	echo loading and updating dotfiles
-	git -C "${HOME}"/dotfiles reset --quiet --hard HEAD
-	git -C "${HOME}"/dotfiles pull --quiet --rebase
-	bash "${HOME}"/dotfiles/install.sh
-fi
+echo loading and updating dotfiles
+git -C "${HOME}"/dotfiles reset --quiet --hard HEAD
+git -C "${HOME}"/dotfiles pull --quiet --rebase
+bash "${HOME}"/dotfiles/install.sh
