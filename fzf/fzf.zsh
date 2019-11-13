e81baf1f0c738f3b3c8649f6b4e72064e01d86eb
Raza Farooq <razaf88@gmail.com>
2019-11-12 22:23:51 -0500

    - Fixed fzf.zsh for root users

--- fzf/fzf.zsh
+++ fzf/fzf.zsh
@@ -1,13 +1,13 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */home/${USER}/.config/fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/home/${USER}/.config/fzf/bin"
+if [[ ! "$PATH" == *${HOME}/.config/fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}${HOME}/.config/fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/home/${USER}/.config/fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "${HOME}/.config/fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/home/${USER}/.config/fzf/shell/key-bindings.zsh"
+source "${HOME}/.config/fzf/shell/key-bindings.zsh"
