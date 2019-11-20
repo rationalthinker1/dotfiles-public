a98dccc0f4ffb50d2ee61f1163739d76ad272389
Raza Farooq <razaf88@gmail.com>
2019-11-19 19:57:01 -0500

    - Added wipe installation in install.sh

--- zsh/reference.zsh
+++ zsh/reference.zsh
@@ -1,6 +1,9 @@
 # Shred a file securely
 shred -zvu -n5 "${file}"
 
+# Shred a directory securely
+wipe -rfi "${folder}"/*
+
 # Creates a 7z securely with password and maximum compression (-mx[1..9])
 7z a -t7z -mhe -mx9 -p "${file}".7z "${folder}"
 
