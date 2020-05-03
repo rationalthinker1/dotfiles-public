6f139e3926e6793c75c2fc41d26dc08013e4db91
Raza Farooq <razaf88@gmail.com>
2020-05-03 16:10:43 -0400

    - Removed install.sh in rc.sh

--- rc.sh
+++ rc.sh
@@ -3,5 +3,5 @@ if [ -n "${SSH_TTY}" ]; then
 	echo loading and updating dotfiles
 	git -C "${HOME}"/.dotfiles reset --quiet --hard HEAD
 	git -C "${HOME}"/.dotfiles pull --quiet --rebase
-	bash "${HOME}"/.dotfiles/install.sh
+	#bash "${HOME}"/.dotfiles/install.sh
 fi
