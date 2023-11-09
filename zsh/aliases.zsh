da1f22e863e7c7c16103f8b9ed6a75002becffe7
Raza Farooq <razaf88@gmail.com>
2023-11-09 11:40:34 -0500

    - Updated Sunlime Text 3 location

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -609,7 +609,7 @@ dbt() {
 if [[ $HOST_OS == "wsl" ]]; then
 	subl() {
 		DISTRO="Ubuntu"
-		SUBLIME_TEXT_LOCATION="/mnt/c/Program Files/Sublime Text 3/subl.exe"
+		SUBLIME_TEXT_LOCATION="/mnt/c/Program Files/Sublime Text/subl.exe"
 		FILE=$1
 		FULL_PATH=$(readlink -f $FILE)
 		$SUBLIME_TEXT_LOCATION "/\/\wsl.localhost\\${DISTRO}${FULL_PATH}"
